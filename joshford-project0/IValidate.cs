55fe35ba7260220326542f2ab56784f3a47e3289
jeford23 <jeford22@gmail.com>
2021-02-17 11:57:37 -0500

    Validate interface modified

--- joshford-project0/IValidate.cs
+++ joshford-project0/IValidate.cs
@@ -8,20 +8,20 @@ namespace joshford_project0
         /// </summary>
         /// <param name="custID"></param>
         /// <returns></returns>
-        public bool CustomerValidate(int custID) => ;
+        public void CustomerValidate(int custID) { };
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="storeID"></param>
         /// <returns></returns>
-        public bool StoreValidate(int storeID) => ;
+        public void StoreValidate(int storeID) { };
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="emplID"></param>
         /// <returns></returns>
-        public bool EmployeeValidate(int emplID) => ;
+        public void EmployeeValidate(int emplID)  { };
     }
 }

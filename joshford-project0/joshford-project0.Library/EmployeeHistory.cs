06fe0975406a8437364e710be86e9a02cdeffe94
jeford23 <jeford22@gmail.com>
2021-02-23 13:15:45 -0500

    Correctly Connected SQL Database to data access library

---
+++ joshford-project0/joshford-project0.Library/EmployeeHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace joshford_project0.Library
+{
+    public partial class EmployeeHistory
+    {
+        public int SaleId { get; set; }
+        public int EmployeeId { get; set; }
+        public int OrderId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
---
+++ joshford-project0/joshford-project0.Library/StoreInventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace joshford_project0.Library
+{
+    public partial class StoreInventory
+    {
+        public int InventoryId { get; set; }
+        public int ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+        public int StoreId { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Store Store { get; set; }
+    }
+}
